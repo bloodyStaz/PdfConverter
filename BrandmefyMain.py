06bd85402045e07c6a07944bd8213da289824e1b
unknown <Luciferxxx81@gmail.com>
2019-03-14 13:49:11 +0500

    ftp remain

--- BrandmefyMain.py
+++ BrandmefyMain.py
@@ -1,5 +1,7 @@
 import sys
 from PyQt5 import QtWidgets,QtCore
+from BrandmefyUpload import MyThread,ProgressThread
+import ftplib
 
 class Windows(QtWidgets.QWidget):
     def __init__(self,state):
@@ -12,6 +14,8 @@ class Windows(QtWidgets.QWidget):
             self.setStyleSheet(fh.read())
         self.state=state
         self.fileOptionState=1
+        self.logoPath=""
+        self.logoTxt="Upload Logo"
 
         #main heading
         self.heading=QtWidgets.QLabel(self)
@@ -87,6 +91,7 @@ class Windows(QtWidgets.QWidget):
         self.logoDropBox.addItem("Top Right")
         self.logoDropBox.addItem("Bottom Left")
         self.logoDropBox.setFixedWidth(150)
+        self.logoDropBox.activated[str].connect(self.uploadLogo)
         
 
         self.logoLabel=QtWidgets.QLabel("Do you want to add logo")
@@ -110,6 +115,11 @@ class Windows(QtWidgets.QWidget):
         self.logoHBox.setContentsMargins(0,0,0,0)
         self.logoGrpBox.setLayout(self.logoHBox)
 
+        self.previewBtn=QtWidgets.QPushButton("Preview")
+        self.previewBtn.setObjectName("browseBtn")
+        self.previewBtn.clicked.connect(lambda:self.startConversion())
+        self.previewBtn.setFixedWidth(103)
+
 
         #Ftp Options
         self.uploadLabel=QtWidgets.QLabel("Upload To System?")
@@ -124,6 +134,7 @@ class Windows(QtWidgets.QWidget):
         self.systemHBox.setContentsMargins(0,0,0,0)
 
         self.uploadBtn=QtWidgets.QPushButton("Upload")
+        self.uploadBtn.clicked.connect(lambda:self.login())
         self.uploadBtn.setObjectName("browseBtn")
         self.deleteBtn=QtWidgets.QPushButton("List videos")
         self.deleteBtn.setObjectName("browseBtn")
@@ -133,6 +144,9 @@ class Windows(QtWidgets.QWidget):
         self.systemBtnHBox.addWidget(self.deleteBtn)
         self.systemBtnHBox.addStretch()
         self.systemBtnHBox.setContentsMargins(0,0,0,0)
+
+        self.progressLabel=QtWidgets.QLabel(self)
+        self.progressLabel.setObjectName("subHeading")
         #Main Layout
         self.layout=QtWidgets.QGridLayout()
         self.layout.addWidget(self.heading,0,0)
@@ -146,8 +160,10 @@ class Windows(QtWidgets.QWidget):
         self.layout.addWidget(self.logoLabel,8,0)
         self.layout.addWidget(self.logoGrpBox,9,0)
         self.layout.addWidget(self.logoDropBox,10,0)
-        self.layout.addLayout(self.systemHBox,11,0)
-        self.layout.addLayout(self.systemBtnHBox,12,0)
+        self.layout.addWidget(self.previewBtn,11,0)
+        self.layout.addLayout(self.systemHBox,12,0)
+        self.layout.addLayout(self.systemBtnHBox,13,0)
+        self.layout.addWidget(self.progressLabel,14,0)
         self.layout.setVerticalSpacing(10)
         
         
@@ -180,11 +196,19 @@ class Windows(QtWidgets.QWidget):
     def logoState(self,option):
         if option.text()=="Yes":
             if option.isChecked()==True:
-                print(self.delayTxtBox.text())
                 self.logoDropBox.show()
         if option.text()=="No":
             if option.isChecked()==True:
                 self.logoDropBox.hide()
+    def uploadLogo(self,text):
+        filter="image (*.img *.jpg)"
+        if len(text)>1 and text!="Upload Logo":
+            self.logoTxt=text
+            self.logoPath=QtWidgets.QFileDialog.getOpenFileName(self,"Browse logo",None,filter)
+            if self.logoPath:
+                self.logoPath=self.logoPath[0]
+                
+            
     def browseFile(self):
         if self.fileOptionState==1:
             filter = "pdf (*.pdf)"
@@ -204,6 +228,76 @@ class Windows(QtWidgets.QWidget):
             if self.filePath:
                 self.filePath=self.filePath[0]
                 self.browsePath.setText(str(self.filePath))
+    def startConversion(self):
+        QtWidgets.QPushButton.setDisabled(self,True)
+        if len(self.delayTxtBox.text())>0:
+            self.thread=MyThread(self.fileOptionState,self.filePath,self.logoTxt,self.logoPath,int(self.delayTxtBox.text()))
+        else:
+            self.thread=MyThread(self.fileOptionState,self.filePath,self.logoTxt,self.logoPath)
+        self.thread.signal.connect(self.progress)
+        self.thread.start()
+        self.progressThread=ProgressThread()
+        self.progressThread.progressSignal.connect(self.progressBar)
+        self.progressThread.start()
+        
+    def progress(self,val):
+        self.progressStatus=val
+    def progressBar(self,val):
+        if self.progressStatus!="Done":
+            self.progressLabel.setText(val)
+        else:
+            QtWidgets.QPushButton.setEnabled(self,True)
+            self.progressLabel.setText("Done")
+            self.progressThread.terminate()
+
+    def uploadTracker(self,block):
+        
+        
+        self.sizeWritten += 1024
+        percentComplete = round((self.sizeWritten / self.totalSize) * 100)
+
+
+        if (self.lastShownPercent != percentComplete):
+            self.lastShownPercent = percentComplete
+
+            self.progresslbl.setText(str(percentComplete) + " percent Complete")
+            QtWidgets.QApplication.processEvents()
+            print(str(percentComplete) + " percent Complete")
+            if percentComplete == 100 :
+                self.progresslbl.setText(str(percentComplete) + " percent Complete \n Your file was send succesfully.")
+
+
+
+    def login(self):
+        
+        try:
+            session=ftplib.FTP()
+            sessionip="192.168.2.100"
+            sessionhost=1026
+            sessionuser="admin"
+            sessionpwd="brandmefy"
+            
+            session.connect(sessionip,sessionhost)
+            session.login(sessionuser,sessionpwd)
+        except:
+            print("Failed to connect please try again later")
+            return None
+        print(session.getwelcome())
+        print("opening file")
+        #print(self.videopath)
+        #self.videopath
+        self.sizeWritten=0
+        self.lastShownPercent=0
+        file=open(self.videopath,"rb")
+        self.totalSize = os.path.getsize(self.videopath)
+
+        #self.txtBox3.text()
+        session.storbinary('STOR {}.mp4'.format(self.txtBox3.text()),file,1024,self.uploadTracker)
+        file.close()
+        print(session.dir())
+        session.quit()
+        
+                    
         
 
 app=QtWidgets.QApplication(sys.argv)
